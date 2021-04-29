1dd734da915de752a34c794038f0fa6f23552580
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-04-29 19:14:12 -0400

    base upload feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.scss';
+import SearchImage from './components/SearchImage/SearchImage';
 
-function App() {
+const App: React.FC = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={SearchImage} />
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
