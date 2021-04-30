a20a881b20b81a18ec03893b6a09f56aedd5e27c
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-04-30 19:13:42 -0400

    modified to show homepage because of file refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.scss';
-import SearchImage from './components/SearchImage/SearchImage';
+import HomePage from './pages/HomePage/HomePage';
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" exact component={SearchImage} />
+                    <Route path="/" exact component={HomePage} />
                 </Switch>
             </BrowserRouter>
         </div>
