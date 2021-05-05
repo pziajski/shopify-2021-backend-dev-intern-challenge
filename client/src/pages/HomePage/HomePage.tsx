263e52951ec08dea9b9e82594fadb013bb0feabf
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-05-05 14:05:02 -0400

    stored base64 string of all images using a recursive filereading and promises

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,13 +2,27 @@ import React, { useState } from "react";
 import ImageList from "../../components/ImageList/ImageList";
 import Navbar from "../../components/Navbar/Navbar";
 import "./HomePage.scss";
+import recursiveFileReader from "../../functions/RecursiveFileReader";
 
 const HomePage: React.FC = () => {
 	const [ uploadedImages, setUploadedImages ] = useState<Array<File> | []>([]);
 
 	const submitUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const files = e.target.files as FileList;
-		const tempArr = new Array<File>();
+		const uploadedList: Array<UploadedImageObject> = [];
+		(recursiveFileReader(uploadedList, files) as Promise<Array<UploadedImageObject>>)
+			.then(arr => {
+				console.log(arr);
+				setUploadedImages([...uploadedImages, ...arr]);
+				axios
+					.post("http://localhost:8000/upload", { images: JSON.stringify(arr) })
+					.then(res => {
+						console.log(res);
+					})
+					.catch(err => {
+						console.log(err);
+					})
+			});
 
 		for (let i = 0; i < files.length; i++) {
 			tempArr.push(files.item(i) as File);
