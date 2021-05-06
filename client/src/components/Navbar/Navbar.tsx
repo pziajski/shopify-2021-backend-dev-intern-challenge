6ef0926c598c274144f27b8726a0e1fc028e13af
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-05-06 14:41:41 -0400

    minor styling and refactor

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,21 +4,12 @@ import "./Navbar.scss";
 const Navbar: React.FC<{ submitUpload: any }> = ({ submitUpload }) => {
 	return (
 		<nav className="navbar">
+			<div>
+				<h1 className="navbar__title">Image Uploader</h1>
+			</div>
 			<form className="navbar__upload">
 				<label htmlFor="upload-input" className="navbar__upload-button">Upload</label>
 				<input id="upload-input" className="navbar__upload-input" type="file" multiple={true} accept=".jpg,.jpeg,.png" onChange={submitUpload} />
-			</form> 
-			<form className="navbar__search">
-				<input type="input" className="navbar__search-input" placeholder="Search" />
-			</form>
-			<form className="navbar__user">
-				<div className="navbar__user-login">
-					<a href="/">Login</a>
-				</div>
-				<div className="navbar__user-signup">
-					<a href="/">Signup</a>
-				</div>
-				
 			</form>
 		</nav>
 	)
