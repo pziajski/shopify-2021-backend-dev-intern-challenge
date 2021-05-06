1169815a9ef6eb551c3fb7099ed3d89de7b953cf
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-05-06 15:59:53 -0400

    added error handling for reading/writing messages

--- server/functions/FileReader.ts
+++ server/functions/FileReader.ts
@@ -2,12 +2,23 @@ import { writeFileSync, readFileSync } from "fs";
 import UploadImagesObject from "../interfaces/UploadImageObject";
 
 export const ReadFromJson = ( path: String ): Array<UploadImagesObject> => {
-	const savedImages = JSON.parse(readFileSync(path as string, "utf8")) as Array<UploadImagesObject>;
+	let savedImages: Array<UploadImagesObject> = [];
+	try {
+		savedImages = JSON.parse(readFileSync(path as string, "utf8")) as Array<UploadImagesObject>;
+	} catch(err) {
+		console.log(`Error reading "${path}", it may not exist...`);
+	}
 	return savedImages;
 }
 
 export const WriteToJson = ( path: String, object: UploadImagesObject )  => {
-	const savedImages = JSON.parse(readFileSync(path as string, "utf8")) as Array<UploadImagesObject>;
+	let savedImages: Array<UploadImagesObject> = [];
+	try {
+		savedImages = JSON.parse(readFileSync(path as string, "utf8")) as Array<UploadImagesObject>;
+	} catch(err) {
+		console.log(`Error reading "${path}", it may not exist...`);
+		console.log(`"${path}" will be created...`);
+	}
 	savedImages.push(object);
 	writeFileSync(path as string, JSON.stringify(savedImages));
 }
